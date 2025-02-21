681654cb91eff719d4b73d165faba7b885a79632
WilsonC7693 <wilsonc7693@student.faytechcc.edu>
2025-02-20 19:36:29 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_WilsonChristian.py
@@ -0,0 +1,15 @@
+# Christian Wilson
+# 2/20/2025
+# P1LAB1
+# How to get input from the user and display output.
+
+
+
+
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
+
+
