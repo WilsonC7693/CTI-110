03c2261d20a9b2ab6b21819279621ea0603fdea4
WilsonC7693 <wilsonc7693@student.faytechcc.edu>
2025-03-08 18:27:59 -0500

    Add files via upload

---
+++ P2LAB2_WilsonChristian.py
@@ -0,0 +1,34 @@
+# Christian Wilson
+# 3/8/2025
+# P2LAB1
+# Code that uses a dictionary to store user input and displays output to the user.
+
+# Creating the dictionary.
+economy_dict = { 'Camaro' : 18.21,
+                 'Prius' : 52.36,
+                 'Model S' : 110,
+                 'Silverado' : 26
+                 }
+
+# Defining keys to equate to the keys of the dictionary.
+keys = economy_dict.keys()
+print(keys) # Printing the value of keys.
+print()# Add a whitespace.
+
+# Prompts the user to enter a vehicle from the list.
+car = input('Enter a vehicle to see it\'s mpg: ' )
+print()# Add a whitespace.
+
+# Matches the key and value to complete a sentence.
+print(f'The {car} gets {economy_dict[car]} mpg')
+print()# Add a whitespace.
+
+# Prompts the user to enter their intended travel distance.
+miles_to_drive = float(input(f'How many miles will you drive the {car} ? '))
+print()# Add a whitespace.
+
+# Defines gallons_needed to eauate to the total of the travel distance divided by the selected vehicles MPG
+gallons_needed = float(miles_to_drive / economy_dict[car])
+print(f'{gallons_needed :.2} gallon(s) are needed to drive the {car} {miles_to_drive} miles.')
+
+
